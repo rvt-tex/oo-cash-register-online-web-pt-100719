38f002213b920ec7d588e994645af73538fa110d
Ruel Tingling <rvtingling@gmail.com>
2019-11-05 12:43:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
     @items = []
   end
 
-  def add_item(item, price, quantity = 1)
+ # def add_item(item, price, quantity = 1)
     @price = price
     @total += price * quantity
     if quantity > 1
